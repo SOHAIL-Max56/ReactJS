d583b9eec06a50d9d95912851efd9852671566b9
Git-Tutorial <mdsohail123g@gamil.com>
2025-08-07 14:49:25 +0530

    Seprate Components

---
+++ src/utlis/constants.js
@@ -0,0 +1,4 @@
+export const LOGO_URL =
+  "https://png.pngtree.com/png-clipart/20200727/original/pngtree-food-delivery-logo-design-png-image_5392526.jpg";
+export const CARD_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
